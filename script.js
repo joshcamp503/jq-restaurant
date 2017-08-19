b18b8ec534d56011604f9e01cae51be22ab3f880
joshcamp503 <joshcamp503@gmail.com>
2017-08-19 16:58:08 -0700

    Initial commit with bulk of work done, but still lacking functionality

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('#content').append(
+    '<div class="header">Ristorante Italiano d\'Oregon</div>',
+    '<div class="navbar"><button id="menu">Menu</button><button id="about">About</button><button id="contact">Contact</button>',
+    '<div id="focal"></div>',
+    '<footer id="footer">This is a footer</footer>'
+    );
+
+  $('button').click(function(){
+
+  })
+});
